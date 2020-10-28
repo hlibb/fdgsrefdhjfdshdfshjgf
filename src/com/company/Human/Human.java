86ab101be3553a02cf3fc49bde6eebe95ed3f6e0
Hlib Astashev <astashev01@gmail.com>
2020-10-28 22:18:21 +0200

    added humans and methods in main()

--- src/com/company/Human/Human.java
+++ src/com/company/Human/Human.java
@@ -63,5 +63,5 @@ public class Human {
                 && h.age == age
                 && h.address == address;
     }
-    
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ import com.company.Human.Human;
 public class Main {
 
     public static void main(String[] args) {
-
+        Human human = new Human("Hlib", "Astashev", "Nab. Peremohy", 19);
+        Human human1 = new Human("Hlib", "Astashev", "Nab. Peremohy", 19);
+        System.out.println(human.toString());
+        if (human.equals(human1)) System.out.println("Oh, it's the same person!");
     }
 }
