dfb27a5de38be0ffa8f6479660b9497291bf1a80
Hlib Astashev <astashev01@gmail.com>
2020-10-28 22:11:26 +0200

    added all needed files to the project

---
+++ src/com/company/Human/Human.java
@@ -0,0 +1,51 @@
+package com.company.Human;
+
+public class Human {
+    private String name;
+    private String surname;
+    private String address;
+    private int age;
+
+    public Human(String name, String surname, String address, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+
+}
